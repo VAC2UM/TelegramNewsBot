567d184efcaa96a6360449b4509e6c2e5e031076
VAC2UM <nikita.golowanev@gmail.com>
2024-03-06 01:09:34 +0300

    Первые тесты добавления новостей

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from os import environ
 
 import requests
 from bs4 import BeautifulSoup
+import time
 
 load_dotenv()
 
@@ -18,6 +19,30 @@ def commands(message):
     bot.send_message(id_channel, message.text)
 
 
-# def parser():
+def parser(back_post_title):
+    URL = "https://www.kinoafisha.info/news/"
+
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    post = soup.find('span', class_='newsV2_title').text.strip()
+
+    if post != back_post_title:
+        # ссылка на описание фильма
+        link = soup.find('div', class_='newsV2_item').a.get('href')
+
+        page2 = requests.get(link)
+        soup2 = BeautifulSoup(page2.content, "html.parser")
+
+        second_post = soup2.find("div", class_="article_column")
+        post_description = soup2.find("div", class_="article_content visualEditorInsertion visualEditorInsertion-large")
+
+        title = second_post.find("h1", class_="article_title").text.strip()
+        description = post_description.find("p").text.strip()
+
+        return f"{title}\n\n{description}\n\n{link}"
+    else:
+        return None, post
+
 
 bot.polling()
--- parser.py
+++ parser.py
@@ -1,35 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
-import urllib.request
 
+
+def parser(back_post_title):
+    URL = "https://www.kinoafisha.info/news/"
+
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    post = soup.find('span', class_='newsV2_title').text.strip()
+
+    if post != back_post_title:
+        # link to film description
+        link = soup.find('div', class_='newsV2_item').a.get('href')
+
+        page2 = requests.get(link)
+        soup2 = BeautifulSoup(page2.content, "html.parser")
+
+        second_post = soup2.find("div", class_="article_column")
+        post_description = soup2.find("div", class_="article_content visualEditorInsertion visualEditorInsertion-large")
+
+        title = second_post.find("h1", class_="article_title").text.strip()
+        description = post_description.find("p").text.strip()
+
+        return f"{title}\n\n{description}\n\n{link}"
+    else:
+        return None, post
+
+
+# main
+with open('last_title.txt', encoding='utf-8', mode='r') as back_post_title:
+    last_title = back_post_title.read()
+    parser(last_title)
+    # реализовать изменение title в txt файле при несовпадении
+
+    # print(last_title)
+    # print(parser(last_title))
+
+
+
+# back_post_title = ""
+# while True:
+#     post_text = parser(back_post_title)
+#     back_post_title = post_text[1]
 #
-# URL = "https://www.kinoafisha.info/news/"
-#
-# page = requests.get(URL)
-# soup = BeautifulSoup(page.content, "html.parser")
-#
-# post = soup.find("a", class_="newsV2_top")
-#
-# title = post.find("span", class_="newsV2_title").text.strip()
-# description = post.find("span", class_="newsV2_desc").text.strip()
-# # url = post.find("span", class_="newsV2_title", href=True)["href"].strip()
+#     if post_text[0] != None:
+#         print(post_text[0])
 
-URL = "https://www.kinoafisha.info/news/"
 
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
-
-# link to film description
-link = soup.find('div', class_='newsV2_item').a.get('href')
-
-page2 = requests.get(link)
-soup2 = BeautifulSoup(page2.content, "html.parser")
-
-post = soup2.find("div", class_="article_column")
-post_description = soup2.find("div", class_="article_content visualEditorInsertion visualEditorInsertion-large")
-
-title = post.find("h1", class_="article_title").text.strip()
-description = post_description.find("p").text.strip()
-print(title + "\n")
-print(description)
-print(link)
+# print(title + "\n")
+# print(description)
+# print(link)
