252373c0433e0dcd54ab1765f9e162120aa4930b
VAC2UM <nikita.golowanev@gmail.com>
2024-03-06 00:14:42 +0300

    Добавлена возможность отправки сообщений через бота

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot
+
 from dotenv import load_dotenv
 from os import environ
 
@@ -11,10 +12,12 @@ token = environ.get('TOKEN')
 id_channel = environ.get('ID_CHANNEL')
 bot = telebot.TeleBot(token)
 
+
 @bot.message_handler(content_types=["text"])
 def commands(message):
     bot.send_message(id_channel, message.text)
 
+
 # def parser():
 
-# bot.polling()
+bot.polling()
