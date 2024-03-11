e3a002637f96fd9d69293fc167d0bd8d874377f2
VAC2UM <nikita.golowanev@gmail.com>
2024-03-11 20:12:27 +0300

    Fixed bug with utf

--- main/bot.py
+++ main/bot.py
@@ -18,7 +18,7 @@ bot = telebot.TeleBot(token)
 # @bot.message_handler(content_types=["text"])
 def commands():
     while True:
-        with open('last_title.txt', encoding='utf-16', mode='r') as back_post_title:
+        with open('last_title.txt', encoding='utf-8', mode='r', errors='ignore') as back_post_title:
             last_title = back_post_title.read()
 
             URL = "https://www.kinoafisha.info/news/"
@@ -30,7 +30,7 @@ def commands():
                 # bot print new post
                 bot.send_message(id_channel, parser(last_title))
 
-                with codecs.open('last_title.txt', 'w', 'utf-16') as f:
+                with codecs.open('last_title.txt', 'w', 'utf-8', errors='ignore') as f:
                     f.write(post)
                     f.close()
             else:
