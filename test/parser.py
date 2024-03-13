4bbb4e630b3734442a97c3fb168c226cc23c67a2
VAC2UM <nikita.golowanev@gmail.com>
2024-03-13 17:03:06 +0300

    Beta test of Letterboxd parser

--- test/parser.py
+++ test/parser.py
@@ -4,6 +4,8 @@ import codecs
 import time
 import re
 
+URL_diary = "https://letterboxd.com/vac2um/films/diary/"
+
 
 def KinoAfisha_parser(back_post_title):
     URL = "https://www.kinoafisha.info/news/"
@@ -30,12 +32,15 @@ def KinoAfisha_parser(back_post_title):
     else:
         return None
 
-def Letterboxd_parser():
-    URL = "https://letterboxd.com/vac2um/films/diary/"
-    response = requests.get(URL)
+def Letterboxd_parser(back_review):
+    URL_my_page = "https://letterboxd.com/Vac2um/"
+
+    response = requests.get(URL_diary)
+    response_my_page = requests.get(URL_my_page)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
+        soup_my_page = BeautifulSoup(response_my_page.text, "html.parser")
 
         link = soup.find('h3', class_='headline-3 prettify').a.get('href')
 
@@ -44,39 +49,77 @@ def Letterboxd_parser():
 
         review = soup2.find('div', class_='review').get_text(strip=True)
 
+        title_tag = soup2.find('span', class_='film-title-wrapper').a
+        title_text = title_tag.text.strip()
+        # title_link = title_tag['href']
+
+        year_tag = soup2.find('small', class_='metadata').a
+        year_text = year_tag.text.strip()
+
         author = "V A C U U M’s review published on Letterboxd:"
+
+        mark = soup_my_page.find('span',class_='rating rated-8').get_text().strip()
+        switch = {
+            "★★★★★": 10,
+            "★★★★½": 9,
+            "★★★★": 8,
+            "★★★½": 7,
+            "★★★": 6,
+            "★★½": 5,
+            "★★": 4,
+            "★½": 3,
+            "★": 2,
+            "½": 1,
+        }
 
         review = review.replace(author,"")
         review = re.sub(r'\.(?=[^\s])', '.\n', review)
-        print(review)
+
+        return f"{title_text + ' '}{year_text}\n\n{review}\n\n{'Моя оценка: '}{switch.get(mark)}{'/10'}"
     else:
         print(f"Failed to retrieve the webpage, status code: {response.status_code}")
 
 
 # реализовать изменение title в txt файле при несовпадении
-# while True:
-#     with open('../main/last_title.txt', encoding='utf-8', mode='r', errors='ignore') as back_post_title:
-#         last_title = back_post_title.read()
-#
-#         URL = "https://www.kinoafisha.info/news/"
-#         page = requests.get(URL)
-#         soup = BeautifulSoup(page.content, "html.parser")
-#
-#         post = soup.find('span', class_='newsV2_title').get_text(strip=True)
-#
-#         while True:
-#             if post != last_title:
-#                 # бот выводит новый пост
-#                 print(KinoAfisha_parser(last_title))
-#
-#                 with codecs.open('../main/last_title.txt', 'w', 'utf-8', errors='ignore') as f:
-#                     f.write(post)
-#                     f.close()
-#                 break
-#             else:
-#                 break
-#         back_post_title.close()
-#     time.sleep(1800)
+while True:
+    with open('../main/last_title.txt', encoding='utf-8', mode='r', errors='ignore') as back_post_title:
+        last_title = back_post_title.read()
 
+        URL = "https://www.kinoafisha.info/news/"
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
 
-Letterboxd_parser()
+        post = soup.find('span', class_='newsV2_title').get_text(strip=True)
+
+        while True:
+            if post != last_title:
+                # бот выводит новый пост
+                print(KinoAfisha_parser(last_title))
+
+                with codecs.open('../main/last_title.txt', 'w', 'utf-8', errors='ignore') as f:
+                    f.write(post)
+                    f.close()
+                break
+            else:
+                break
+        back_post_title.close()
+    with open('../main/last_review.txt', encoding='utf-8', mode='r', errors='ignore') as back_review_title:
+        last_review = back_review_title.read()
+
+        page = requests.get(URL_diary)
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        review = soup.find('h3', class_='headline-3 prettify').a
+        review_text = review.text.strip()
+
+        if review_text != last_review:
+            print(Letterboxd_parser(last_review))
+
+            with codecs.open('../main/last_review.txt', 'w', 'utf-8', errors='ignore') as f:
+                f.write(review_text)
+                f.close()
+            break
+        else:
+            print("Review has not updated")
+            break
+    time.sleep(1800)
